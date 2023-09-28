181f1711f03389fa4bd2979cfc91c8e2ddea66c6
CADEN <cadenhendersonucf@Knights.ucf.edu>
2023-09-28 15:42:04 -0400

    gun new attempt

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -13,21 +13,88 @@ using UnityEngine;
  * 
  */
 
+public class Bullet
+{
+    public Vector3 position;
+    public Vector3 direction;
+    public float distanceTraveled;
+    public float maxDistance;
+    public int currBounces;
+    public int maxBounces;
+    public float speed;
+    public int bulletIndex;
+
+    public Bullet(Vector3 pos, Vector3 dir)
+    {
+        position = pos;
+        direction = dir;
+    }
+
+    public Bullet(int index, float speed, int bounces, float dist)
+    {
+        bulletIndex = index;
+        this.speed = speed;
+        currBounces = bounces;
+        distanceTraveled = dist;
+    }
+
+    public Bullet() { }
+
+    public IEnumerator BulletMove(Transform source, LineRenderer lineRenderer)
+    {
+        Bullet bullet = new Bullet();
+        bullet.currBounces = 0;
+        bullet.position = new Vector3(source.position.x, 2f, source.position.z);
+        bullet.direction = new Vector3(source.forward.x, 0f, source.forward.z);
+        bullet.distanceTraveled = 0f;
+
+        while (bullet.currBounces < maxBounces && bullet.distanceTraveled < maxDistance)
+        {
+            //move bullet in its fired direction
+            bullet.position = Vector3.MoveTowards(
+                    bullet.position,
+                    bullet.position + bullet.direction,
+                    speed * Time.deltaTime);
+
+
+            //track how far bullet has traveled so we know when to kill it
+            bullet.distanceTraveled += speed * Time.deltaTime;
+
+            RaycastHit hitData;
+
+            //if hits object, ricochet
+            if (Physics.Raycast(bullet.position, bullet.direction, out hitData, 0.01f))
+            {
+                bullet.direction = hitData.normal;
+                maxBounces++;
+            }
+
+            //update bullet trail line
+            lineRenderer.SetPosition(bulletIndex, bullet.position);
+
+            //wait until end of frame to continue while loop
+            yield return null;
+        }
+
+        Debug.Log("bullet stopped");
+    }
+}
+
 
 public class GunController : MonoBehaviour
 {
     //public vars
     public int maxBounces;
     public float maxDistance;
+    public float bulletSpeed;
 
     [Tooltip("Time in seconds while player is motionless firing gun.")]
     public float fireTime;
 
     //private vars
     private bool canShoot = true;
-    private int currBounces;
-    private Vector3 bulletPos;
-    private Vector3 bulletDir;
+    Bullet headBullet;
+    Bullet trailingBullet;
 
     //references
     private LineRenderer _lineRenderer;
@@ -35,6 +102,8 @@ public class GunController : MonoBehaviour
     private void Awake()
     {
         _lineRenderer = GetComponent<LineRenderer>();
+        headBullet = new Bullet(1, bulletSpeed, maxBounces, maxDistance);
+        trailingBullet = new Bullet(0, bulletSpeed, maxBounces, maxDistance);
     }
 
     private void Update()
@@ -56,12 +125,35 @@ public class GunController : MonoBehaviour
 
     private void FireGun()
     {
+        _lineRenderer.positionCount = 2;
+        _lineRenderer.SetPosition(0, new Vector3(gameObject.transform.position.x, 2f, gameObject.transform.position.z));
+
+        StartCoroutine(FireRoutine());
+    }
+
+    private IEnumerator FireRoutine()
+    {
+        StartCoroutine(headBullet.BulletMove(gameObject.transform, _lineRenderer));
+
+        //delay to create separation between front and back of bullet trail
+        yield return new WaitForSeconds(0.1f);
+
+        StartCoroutine(trailingBullet.BulletMove(gameObject.transform, _lineRenderer));
+    }
+
+
+
+    /*
+    private void FireGunOld()
+    {
         _lineRenderer.positionCount = 1;
         currBounces = 0;
-        bulletPos = gameObject.transform.position;
+        bulletFirePos = gameObject.transform.position;
+        bulletFirePos = new Vector3(bulletFirePos.x, 2f, bulletFirePos.z);
         bulletDir = gameObject.transform.forward;
+        bulletDir = new Vector3(bulletDir.x, 0f, bulletDir.z);
 
-        _lineRenderer.SetPosition(0, bulletPos);
+        _lineRenderer.SetPosition(0, bulletFirePos);
 
         RaycastHit hitData;
 
@@ -71,7 +163,7 @@ public class GunController : MonoBehaviour
             _lineRenderer.positionCount++;
 
             //if hits something
-            if(Physics.Raycast(bulletPos, bulletDir, out hitData, maxDistance))
+            if(Physics.Raycast(bulletFirePos, bulletDir, out hitData, maxDistance))
             {
                 //add point for bullet trail
                 _lineRenderer.SetPosition(currBounces + 1, hitData.point);
@@ -96,12 +188,13 @@ public class GunController : MonoBehaviour
             }
         }
     }
+    */
 
     private IEnumerator FreezePlayerRoutine()
     {
-        PlayerController.main.firingGun = true;
+        //PlayerController.main.firingGun = true;
         yield return new WaitForSeconds(fireTime);
-        PlayerController.main.firingGun = false;
+        //PlayerController.main.firingGun = false;
     }
 
     public void DisableShooting()
