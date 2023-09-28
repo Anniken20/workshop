0d5bb7baeaa7456f9113952b0fad04e2cc40af8a
CADEN <cadenhendersonucf@Knights.ucf.edu>
2023-09-28 15:58:58 -0400

    more attempts

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -40,43 +40,54 @@ public class Bullet
 
     public Bullet() { }
 
+    public override string ToString()
+    {
+        return "index: " + bulletIndex + ", pos: " + position + ", bounces: " + currBounces;
+    }
+
     public IEnumerator BulletMove(Transform source, LineRenderer lineRenderer)
     {
-        Bullet bullet = new Bullet();
-        bullet.currBounces = 0;
-        bullet.position = new Vector3(source.position.x, 2f, source.position.z);
-        bullet.direction = new Vector3(source.forward.x, 0f, source.forward.z);
-        bullet.distanceTraveled = 0f;
+        currBounces = 0;
+        position = new Vector3(source.position.x, 2f, source.position.z);
+        direction = new Vector3(source.forward.x, 0f, source.forward.z);
+        distanceTraveled = 0f;
 
-        while (bullet.currBounces < maxBounces && bullet.distanceTraveled < maxDistance)
+        while (currBounces < maxBounces && distanceTraveled < maxDistance)
         {
             //move bullet in its fired direction
-            bullet.position = Vector3.MoveTowards(
-                    bullet.position,
-                    bullet.position + bullet.direction,
+            position = Vector3.MoveTowards(
+                    position,
+                    position + direction,
                     speed * Time.deltaTime);
 
 
             //track how far bullet has traveled so we know when to kill it
-            bullet.distanceTraveled += speed * Time.deltaTime;
+            distanceTraveled += speed * Time.deltaTime;
 
             RaycastHit hitData;
 
             //if hits object, ricochet
-            if (Physics.Raycast(bullet.position, bullet.direction, out hitData, 0.01f))
+            if (Physics.Raycast(position, direction, out hitData, 0.01f))
             {
-                bullet.direction = hitData.normal;
+                direction = hitData.normal;
                 maxBounces++;
             }
 
             //update bullet trail line
-            lineRenderer.SetPosition(bulletIndex, bullet.position);
+            lineRenderer.SetPosition(bulletIndex, position);
 
             //wait until end of frame to continue while loop
             yield return null;
         }
+        if(currBounces < maxBounces)
+        {
+            Debug.Log("bullet stopped due to bounces");
+        }
+        if (distanceTraveled < maxDistance)
+        {
+            Debug.Log("bullet stopped due to bounces");
+        }
 
-        Debug.Log("bullet stopped");
     }
 }
 
@@ -109,6 +120,8 @@ public class GunController : MonoBehaviour
     private void Update()
     {
         GetInput();
+        Debug.Log("Head: " + headBullet);
+        Debug.Log("Tail: " + trailingBullet);
     }
 
     private void GetInput()
@@ -140,55 +153,6 @@ public class GunController : MonoBehaviour
 
         StartCoroutine(trailingBullet.BulletMove(gameObject.transform, _lineRenderer));
     }
-
-
-
-    /*
-    private void FireGunOld()
-    {
-        _lineRenderer.positionCount = 1;
-        currBounces = 0;
-        bulletFirePos = gameObject.transform.position;
-        bulletFirePos = new Vector3(bulletFirePos.x, 2f, bulletFirePos.z);
-        bulletDir = gameObject.transform.forward;
-        bulletDir = new Vector3(bulletDir.x, 0f, bulletDir.z);
-
-        _lineRenderer.SetPosition(0, bulletFirePos);
-
-        RaycastHit hitData;
-
-        //loop for bounces
-        while(currBounces < maxBounces)
-        {
-            _lineRenderer.positionCount++;
-
-            //if hits something
-            if(Physics.Raycast(bulletFirePos, bulletDir, out hitData, maxDistance))
-            {
-                //add point for bullet trail
-                _lineRenderer.SetPosition(currBounces + 1, hitData.point);
-
-                //end ricochet if you hit not-reflective
-                if (hitData.collider.gameObject.tag == "NotRicochet")
-                {
-                    break;
-                }
-
-                //ricochet
-                currBounces++;
-                bulletPos = hitData.point;
-                bulletDir = hitData.normal;
-            } 
-
-            //else doesn't hit something
-            else
-            {
-                _lineRenderer.SetPosition(currBounces + 1, bulletDir * maxDistance);
-                break;
-            }
-        }
-    }
-    */
 
     private IEnumerator FreezePlayerRoutine()
     {
