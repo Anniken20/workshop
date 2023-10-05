8344f69bea01acb0ba96da2ff002d8843ddc79d9
CADEN <cadenhendersonucf@Knights.ucf.edu>
2023-10-05 14:23:09 -0400

    camera transition tweaks
    + editable blend duration

--- Assets/AnnikenTestScripts/CameraController.cs
+++ Assets/AnnikenTestScripts/CameraController.cs
@@ -6,13 +6,9 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    public CinemachineBrain camBrain;
     public CinemachineVirtualCamera mainCam;
     public CinemachineVirtualCamera shoulderCam;
-    /*
-    public GameObject camera;
-    public GameObject isometricCamPosition;
-    public GameObject thirdPersonCamPosition;
-    */
 
     public float switchViewDuration;
 
@@ -20,8 +16,8 @@ public class CameraController : MonoBehaviour
 
     private void Start()
     {
-        //align it with the proper place
-        //camera.transform.position = isometricCamPosition.transform.position;
+        //set transition time of cinemachine camera blend
+        camBrain.m_DefaultBlend.m_Time = switchViewDuration;
     }
 
     private void Update()
@@ -29,28 +25,19 @@ public class CameraController : MonoBehaviour
         // Button = switch
         if (Input.GetKeyDown(KeyCode.C))
         {
+            //flip isometric bool
             isIsometricView = !isIsometricView;
 
-            // Toggle camera iso and over shoulder
-            // Move to proper target position with easing
+            //change cinemachine virtual cam priorities.
+            //cinemachine brain will automatically "blend" to the highest priority camera.
             if (isIsometricView)
             {
-                Debug.Log("Moving to isoCamPos");
                 mainCam.Priority = 0;
                 shoulderCam.Priority = 1;
-                /*
-                camera.transform.DOMove(isometricCamPosition.transform.position,
-                    switchViewDuration).SetEase(Ease.InOutBack);
-                */
             } else
             {
-                Debug.Log("Moving to shoulderCamPos");
                 mainCam.Priority = 1;
                 shoulderCam.Priority = 0;
-                /*
-                camera.transform.DOMove(thirdPersonCamPosition.transform.position,
-                    switchViewDuration).SetEase(Ease.InOutBack);
-                */
             }
         }
     }
