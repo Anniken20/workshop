da5993e68b3feb117da8ec964215ade71e96eae0
Amadin <111744298+Anniken20@users.noreply.github.com>
2023-10-05 02:23:40 +0200

    Main menu with scenes and iso to over shoulder

--- Assets/AnnikenTestScripts/CombatSystem.cs
+++ Assets/AnnikenTestScripts/CombatSystem.cs
@@ -1,22 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CombatSystem : MonoBehaviour
 {
-    public CameraController cameraController; // Reference to the camera controller.
+    public GameObject player;
+    public GameObject enemy;
 
-    // Start combat and switch to over-the-shoulder view.
-    public void StartCombat()
+    private bool isPlayerTurn = true;
+    private bool combatEnded = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        cameraController.SwitchToOverShoulderView();
-        // Add combat initialization logic here.
+        // Begin combat with the player's turn
+        isPlayerTurn = true;
+        combatEnded = false;
+        Debug.Log("Combat begins! Player's turn.");
     }
 
-    // End combat and switch back to isometric view.
-    public void EndCombat()
+    private void Update()
     {
-        cameraController.SwitchToIsometricView();
-        // Add combat ending logic here.
+        if (!combatEnded)
+        {
+            if (isPlayerTurn)
+            {
+                // Player's turn
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    // Playrer attack
+                    int playerDamage = UnityEngine.Random.Range(10, 20);
+                    Debug.Log("Player attacks for " + playerDamage + " damage!");
+                    enemy.GetComponent<Health>().TakeDamage(playerDamage);
+
+                    isPlayerTurn = false;
+                }
+            }
+            else
+            {
+                // Enemy's turn
+                int enemyDamage = UnityEngine.Random.Range(5, 15);
+                Debug.Log("Enemy attacks for " + enemyDamage + " damage!");
+                player.GetComponent<Health>().TakeDamage(enemyDamage);
+
+                isPlayerTurn = true;
+            }
+
+            // Check if combat has ended
+            if (player.GetComponent<Health>().IsDead())
+            {
+                Debug.Log("Player is defeated. Enemy wins!");
+                combatEnded = true;
+            }
+            else if (enemy.GetComponent<Health>().IsDead())
+            {
+                Debug.Log("Enemy is defeated. Player wins!");
+                combatEnded = true;
+            }
+        }
+    }
+}
+
+// Example Health script
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+    }
+
+    public bool IsDead()
+    {
+        return currentHealth <= 0;
     }
 }
---
+++ Assets/Main Game scenes/MainMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadHub()
+    {
+        SceneManager.LoadScene("HUB");
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void QuitGame() 
+    {
+        Debug.Log("Button pressed");
+        Application.Quit();
+    }
+
+}
