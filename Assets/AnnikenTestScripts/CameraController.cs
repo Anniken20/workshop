ba6ef02dc7b02173a33edba7b87d3a24f4f16873
CADEN <cadenhendersonucf@Knights.ucf.edu>
2023-10-05 14:09:29 -0400

    smooth camera transition

--- Assets/AnnikenTestScripts/CameraController.cs
+++ Assets/AnnikenTestScripts/CameraController.cs
@@ -2,21 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    public CinemachineVirtualCamera mainCam;
+    public CinemachineVirtualCamera shoulderCam;
+    /*
     public GameObject camera;
     public GameObject isometricCamPosition;
     public GameObject thirdPersonCamPosition;
+    */
 
     public float switchViewDuration;
 
-    private bool isIsometricView = true;
+    private bool isIsometricView = false;
 
     private void Start()
     {
         //align it with the proper place
-        camera.transform.position = isometricCamPosition.transform.position;
+        //camera.transform.position = isometricCamPosition.transform.position;
     }
 
     private void Update()
@@ -31,13 +36,21 @@ public class CameraController : MonoBehaviour
             if (isIsometricView)
             {
                 Debug.Log("Moving to isoCamPos");
+                mainCam.Priority = 0;
+                shoulderCam.Priority = 1;
+                /*
                 camera.transform.DOMove(isometricCamPosition.transform.position,
                     switchViewDuration).SetEase(Ease.InOutBack);
+                */
             } else
             {
                 Debug.Log("Moving to shoulderCamPos");
+                mainCam.Priority = 1;
+                shoulderCam.Priority = 0;
+                /*
                 camera.transform.DOMove(thirdPersonCamPosition.transform.position,
                     switchViewDuration).SetEase(Ease.InOutBack);
+                */
             }
         }
     }
