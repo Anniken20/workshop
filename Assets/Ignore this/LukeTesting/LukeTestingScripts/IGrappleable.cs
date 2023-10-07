80e941f10b7037643d2a5b9b496648fcd793067a
Amadin <111744298+Anniken20@users.noreply.github.com>
2023-10-07 06:26:31 +0200

    Revert "Anniken test"

--- Assets/Ignore this/LukeTesting/LukeTestingScripts/IGrappleable.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IGrappleable{
-    void Grappled(bool active, GameObject hitObject, Transform startPos);
-}
