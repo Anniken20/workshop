b75f680dfcb5613a03a6da0a0ec786c6cc13e6d6
Amadin <111744298+Anniken20@users.noreply.github.com>
2023-10-27 01:27:39 +0200

    ai

--- Assets/AnnikenTestScripts/AIAttack.cs
+++ Assets/AnnikenTestScripts/AIAttack.cs
@@ -17,13 +17,11 @@ public class AiAttack : MonoBehaviour
         {
             case "Idle":
                 // Perform actions for the "Idle" state
-                // Example: AI scans the environment for targets
                 SetState("Searching");
                 break;
 
             case "Searching":
                 // Perform actions for the "Searching" state
-                // Example: AI looks for potential enemies
                 if (EnemyDetected())
                 {
                     SetState("Engaging");
@@ -32,7 +30,6 @@ public class AiAttack : MonoBehaviour
 
             case "Engaging":
                 // Perform actions for the "Engaging" state
-                // Example: Decide how to engage the enemy (launch, AoE, lob, melee, slam, shot)
                 if (ShouldLaunch())
                 {
                     SetState("Launching");
@@ -60,8 +57,6 @@ public class AiAttack : MonoBehaviour
                 break;
 
             case "Launching":
-                // Perform actions for the "Launching" state
-                // Example: AI launches a projectile
                 // Transition to a new state when the action is complete
                 SetState("Idle");
                 break;
@@ -69,47 +64,45 @@ public class AiAttack : MonoBehaviour
             // Implement similar blocks for other states (e.g., AoEAttack, Lobbing, MeleeAttacking, Slamming, Shooting)
         }
     }
-
-    // Mock functions for conditions
     private bool EnemyDetected()
     {
         // Implement logic to detect enemies
-        return true; // Replace with your condition
+        return true; 
     }
 
     private bool ShouldLaunch()
     {
         // Implement logic for deciding whether to launch
-        return true; // Replace with your condition
+        return true; 
     }
 
     private bool ShouldAoE()
     {
         // Implement logic for deciding whether to use AoE attack
-        return false; // Replace with your condition
+        return false;
     }
 
     private bool ShouldLob()
     {
         // Implement logic for deciding whether to lob
-        return false; // Replace with your condition
+        return false; 
     }
 
     private bool ShouldMelee()
     {
         // Implement logic for deciding whether to melee attack
-        return false; // Replace with your condition
+        return false; 
     }
 
     private bool ShouldSlam()
     {
         // Implement logic for deciding whether to slam
-        return false; // Replace with your condition
+        return false; 
     }
 
     private bool ShouldShot()
     {
         // Implement logic for deciding whether to shoot
-        return false; // Replace with your condition
+        return false; 
     }
 }
