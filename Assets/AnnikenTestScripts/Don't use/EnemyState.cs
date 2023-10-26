07e635116e0162b10d38875792c604a836071690
Amadin <111744298+Anniken20@users.noreply.github.com>
2023-10-26 02:02:16 +0200

    Buncha AI things

---
+++ Assets/AnnikenTestScripts/Don't use/EnemyState.cs
@@ -0,0 +1,21 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Amadin.FSM
+{
+    public enum EnemyState
+    {
+        Idle,
+        Chase,
+        Melee,
+        Charge,
+        Shoot,
+        Bounce,
+        Cover,
+        Patrol,
+        Evade,
+        Hide,
+        Die
+    }
+}
---
+++ Assets/AnnikenTestScripts/Don't use/StateEvent.cs
@@ -0,0 +1,13 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Amadin.FSM
+{
+    public enum StateEvent
+    {
+        DetecctPlayer,
+        LostPlayer,
+        ChargeImpact
+    }
+}
