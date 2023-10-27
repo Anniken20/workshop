f86d3a6e736f964970e77c85ae2032b9ff86be68
bvo29 <bvocubs@gmail.com>
2023-10-27 00:27:33 -0400

    Breakable Objects update with rigidbody timer

--- Assets/BrianTestScripts/BreakController.cs
+++ Assets/BrianTestScripts/BreakController.cs
@@ -4,9 +4,84 @@ using UnityEngine;
 
 public class BreakController : MonoBehaviour
 {
-     public GameObject[] fragments; // An array to store the smaller fragments in the Inspector
+    public GameObject[] fragments; // An array to store the smaller fragments in the Inspector
     public float explosionForce = 10f;
     public float explosionRadius = 5f;
+    public float disableRigidbodyDelay = 30f; // Time before disabling rigidbodies
+    public float enableRigidbodyDelay = 30f; // Time to enable rigidbodies again if not motionless
+
+    private Rigidbody[] fragmentRigidbodies;
+    private Vector3[] initialFragmentPositions;
+    private bool rigidbodiesDisabled = false;
+    private float lastMovementTime;
+
+    private void Start()
+    {
+        // Check if there are fragments assigned
+        if (fragments != null && fragments.Length > 0)
+        {
+            // Initialize arrays and capture initial positions
+            fragmentRigidbodies = new Rigidbody[fragments.Length];
+            initialFragmentPositions = new Vector3[fragments.Length];
+
+            for (int i = 0; i < fragments.Length; i++)
+            {
+                GameObject fragment = fragments[i];
+                fragmentRigidbodies[i] = fragment.GetComponent<Rigidbody>();
+                initialFragmentPositions[i] = fragment.transform.position;
+            }
+        }
+        else
+        {
+            Debug.LogError("Fragments not assigned in the Inspector.");
+        }
+    }
+
+    private void Update()
+    {
+        if (!rigidbodiesDisabled)
+        {
+            bool fragmentsAreMotionless = true;
+
+            // Check if any fragment has moved
+            for (int i = 0; i < fragmentRigidbodies.Length; i++)
+            {
+                if (fragmentRigidbodies[i] != null && fragmentRigidbodies[i].IsSleeping())
+                {
+                    lastMovementTime = Time.time;
+                }
+                else
+                {
+                    fragmentsAreMotionless = false;
+                }
+            }
+
+            if (fragmentsAreMotionless && Time.time - lastMovementTime > disableRigidbodyDelay)
+            {
+                // Disable rigidbodies after 30 seconds of inactivity
+                for (int i = 0; i < fragmentRigidbodies.Length; i++)
+                {
+                    if (fragmentRigidbodies[i] != null)
+                    {
+                        fragmentRigidbodies[i].isKinematic = true;
+                    }
+                }
+                rigidbodiesDisabled = true;
+            }
+        }
+        else if (Time.time - lastMovementTime <= enableRigidbodyDelay)
+        {
+            // If fragments become active within 30 seconds after disabling, re-enable rigidbodies
+            for (int i = 0; i < fragmentRigidbodies.Length; i++)
+            {
+                if (fragmentRigidbodies[i] != null)
+                {
+                    fragmentRigidbodies[i].isKinematic = false;
+                }
+            }
+            rigidbodiesDisabled = false;
+        }
+    }
 
     public void BreakIntoPieces()
     {
