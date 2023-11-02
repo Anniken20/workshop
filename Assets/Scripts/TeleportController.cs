c42f5b09e3baa3a0851ff13f6670a217f0400953
bvo29 <bvocubs@gmail.com>
2023-11-02 17:51:17 -0400

    WallToPoint

--- Assets/Scripts/TeleportController.cs
+++ Assets/Scripts/TeleportController.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class TeleportController : MonoBehaviour
 {
-    public GameObject toPoint;
+   public GameObject toPoint;
+    private CharacterController characterController;
+    private bool isTransitioning = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.CompareTag("Player") && !isTransitioning)
         {
-            other.gameObject.GetComponent<CharacterController>().enabled = false;
-            other.gameObject.transform.position = toPoint.transform.position;
-            other.gameObject.GetComponent<CharacterController>().enabled = true;
+            characterController = other.gameObject.GetComponent<CharacterController>();
+            StartCoroutine(TransitionThroughDoor(other.gameObject));
         }
+    }
+
+    IEnumerator TransitionThroughDoor(GameObject player)
+    {
+        isTransitioning = true;
+
+        characterController.enabled = false;
+
+        //Determines direction player is facing
+        Vector3 playerForward = player.transform.forward;
+
+        //Calculates destination position
+        Vector3 destination = toPoint.transform.position;
+
+        //Calculates midpoint through doorway
+        Vector3 midpoint = (player.transform.position + destination) / 2f;
+
+        float transitionDuration = 1.0f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < transitionDuration)
+        {
+            //Interpolates position to create smoother transition
+            player.transform.position = Vector3.Lerp(player.transform.position, midpoint, elapsedTime / transitionDuration);
+
+            //Rotates player to face same direction
+            player.transform.forward = playerForward;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        player.transform.position = destination;
+
+        characterController.enabled = true;
+
+        isTransitioning = false;
     }
 }
