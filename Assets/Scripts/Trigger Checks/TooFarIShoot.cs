13283a2480522914f1dc808b22c12058a81acde6
Amadin <111744298+Anniken20@users.noreply.github.com>
2023-11-15 04:07:30 +0100

    Sheriff
    hide and shoot

---
+++ Assets/Scripts/Trigger Checks/TooFarIShoot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooFarIShoot : MonoBehaviour
+{
+    private Sheriff s;
+
+    void Start()
+    {
+        s = GetComponentInParent<Sheriff>();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("pew");
+            s.startshooting();
+        }
+        
+    }
+
+}
