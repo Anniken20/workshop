15e4818cf1d2ad114a5bb8ae4c07a51076e994c6
CADEN <cadenhendersonucf@Knights.ucf.edu>
2023-11-18 19:46:37 -0500

    more work on dialogue popup

--- Assets/Scripts/NPC/DialoguePopupController.cs
+++ Assets/Scripts/NPC/DialoguePopupController.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
 
 [System.Serializable]
 public struct DialogueFrame
 {
     public Sprite portrait;
     public string message;
+    [Tooltip("The time it takes between each character")]
+    public float writeWaitTime;
     public UnityEvent onWriteEvent;
 }
 
 public class DialoguePopupController : MonoBehaviour
 {
-    public GameObject dialogueObject;
+    public GameObject dialoguePanel;
+    public Sprite characterPortrait;
+    public TMP_Text characterNameText;
+    public TMP_Text characterText;
+
+    public string characterName;
     public DialogueFrame[] dialogues;
 
     private int dialogueIndex = 0;
@@ -27,17 +35,31 @@ public class DialoguePopupController : MonoBehaviour
     public void BeginSpeaking()
     {
         dialogueIndex = 0;
-        dialogueObject.SetActive(true);
+        dialoguePanel.SetActive(true);
+        characterNameText.text = characterName;
         DisplayDialoguePiece(dialogueIndex);
     }
     
     public void StopSpeaking()
     {
-        dialogueObject.SetActive(false);
+        dialoguePanel.SetActive(false);
     }
 
     private void DisplayDialoguePiece(int i)
     {
+        dialogues[i].onWriteEvent.Invoke();
+        characterPortrait = dialogues[i].portrait;
+        StartCoroutine(WriteRoutine(dialogues[i].message, dialogues[i].writeWaitTime));
+    }
 
+    private IEnumerator WriteRoutine(string msg, float waitTime)
+    {
+        int i = 0;
+        while(i < msg.Length)
+        {
+            yield return new WaitForSeconds(waitTime);
+            characterText.text += msg[i];
+            i++;
+        }
     }
 }
