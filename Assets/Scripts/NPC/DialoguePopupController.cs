69a41cfb52cc0c0bc8b8320d03eacd50c632cd10
CADEN <cadenhendersonucf@Knights.ucf.edu>
2023-11-19 16:27:42 -0500

    NPC dialogue popup complete

--- Assets/Scripts/NPC/DialoguePopupController.cs
+++ Assets/Scripts/NPC/DialoguePopupController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using TMPro;
+using UnityEngine.UI;
+using DG.Tweening;
+using StarterAssets;
 
 [System.Serializable]
 public struct DialogueFrame
@@ -16,15 +19,19 @@ public struct DialogueFrame
 
 public class DialoguePopupController : MonoBehaviour, IInteractable
 {
+    [Header("References")]
     public GameObject dialoguePanel;
-    public Sprite characterPortrait;
+    public Image characterPortrait;
     public TMP_Text characterNameText;
     public TMP_Text characterText;
+    public ThirdPersonController player;
 
+    [Header("Dialogue")]
     public string characterName;
     public DialogueFrame[] dialogues;
 
     private int dialogueIndex = 0;
+    private Coroutine writeRoutine;
 
     public void Interacted()
     {
@@ -40,6 +47,10 @@ public class DialoguePopupController : MonoBehaviour, IInteractable
     public void GoNext()
     {
         dialogueIndex++;
+        if(dialogueIndex >= dialogues.Length)
+        {
+            StopSpeaking();
+        }
         DisplayDialoguePiece(dialogueIndex);
     }
 
@@ -47,24 +58,30 @@ public class DialoguePopupController : MonoBehaviour, IInteractable
     {
         dialogueIndex = 0;
         dialoguePanel.SetActive(true);
+        player._inDialogue = true;
+        dialoguePanel.transform.localScale = new Vector3(0f, 0f);
+        dialoguePanel.transform.DOScale(new Vector3(1, 1), 1f).SetEase(Ease.OutExpo);
         characterNameText.text = characterName;
         DisplayDialoguePiece(dialogueIndex);
     }
     
     public void StopSpeaking()
-    {
+    { 
+        player._inDialogue = false;
         dialoguePanel.SetActive(false);
     }
 
     private void DisplayDialoguePiece(int i)
     {
         dialogues[i].onWriteEvent.Invoke();
-        characterPortrait = dialogues[i].portrait;
-        StartCoroutine(WriteRoutine(dialogues[i].message, dialogues[i].writeWaitTime));
+        characterPortrait.sprite = dialogues[i].portrait;
+        if (writeRoutine != null) StopCoroutine(writeRoutine);
+        writeRoutine = StartCoroutine(WriteRoutine(dialogues[i].message, dialogues[i].writeWaitTime));
     }
 
     private IEnumerator WriteRoutine(string msg, float waitTime)
     {
+        characterText.text = "";
         int i = 0;
         while(i < msg.Length)
         {
