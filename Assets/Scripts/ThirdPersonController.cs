17b6374b62808606e8e6a15a7feadec224206b47
CADEN <cadenhendersonucf@Knights.ucf.edu>
2023-11-20 21:46:19 -0500

    movement lock bug fix

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -448,7 +448,15 @@ namespace StarterAssets
         //for example, unpausing shouldn't unlock from luna's redirect movement lock
         private bool IsMovementLocked()
         {
-            return _paused || _lunaLocked || _stunned || !_canMove || _inDialogue || _manipulatingLasso;
+            //dont delete this stuff because it's helpful debug.
+            /*
+            Debug.Log("_paused: " + _paused);
+            Debug.Log("_lunaLocked: " + _lunaLocked);
+            Debug.Log("!_canMove: " + !_canMove);
+            Debug.Log("_inDialogue: " + _inDialogue);
+            Debug.Log("_manipulatingLasso: " + _manipulatingLasso);
+            */
+            return _paused || _lunaLocked || _stunned || _inDialogue || _manipulatingLasso;
         }
 
         private void OnEnable(){
