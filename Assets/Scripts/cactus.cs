c1e2832fc6076c9db94ea53ef185c953ea05b06b
LucasP0326 <2002lucassanchez@gmail.com>
2023-11-27 16:41:37 -0500

    Cooking
    Just ya wait

---
+++ Assets/Scripts/cactus.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cactus : MonoBehaviour
+{
+    /*public int damageAmount = 10; // Adjust the damage amount as needed
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Assuming the player has a health script
+            HealthScript playerHealth = other.GetComponent<HealthScript>();
+
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damageAmount);
+            }
+        }
+    }*/
+}
