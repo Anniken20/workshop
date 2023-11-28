5cac7d9896b32efb28bfcad0f140e2f857839db0
LucasP0326 <2002lucassanchez@gmail.com>
2023-11-27 19:19:14 -0500

    Partial UI Commit Stuff
    Doing some UI and map stuff

--- Assets/Scripts/cactus.cs
+++ Assets/Scripts/cactus.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class cactus : MonoBehaviour
 {
-    /*public int damageAmount = 10; // Adjust the damage amount as needed
+    public int damageAmount = 10; // Adjust the damage amount as needed
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             // Assuming the player has a health script
-            HealthScript playerHealth = other.GetComponent<HealthScript>();
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
 
             if (playerHealth != null)
             {
                 playerHealth.TakeDamage(damageAmount);
             }
         }
-    }*/
+    }
 }
