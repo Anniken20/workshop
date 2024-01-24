800e627af3a748c3706bf19cf84ff795791e8702
CADEN <cadenhendersonucf@Knights.ucf.edu>
2024-01-24 00:31:51 -0500

    Splines package + Minecart wip

--- Assets/Scripts/Level Mechanics/MinecartMechanic.cs
+++ Assets/Scripts/Level Mechanics/MinecartMechanic.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.Splines;
 
 public class MinecartMechanic : OnPlayerHit
 {
+    private SplineAnimate splineAnimator;
     public override void HitEffect(Collision col)
     {
         Debug.Log("Stepped on minecart");
+        splineAnimator = GetComponent<SplineAnimate>();
         col.transform.SetParent(transform);
-        transform.DOMoveZ(transform.position.z - 10f, 2f);
+        //transform.DOMoveZ(transform.position.z - 10f, 2f);
+
+        splineAnimator.Play();
+    }
+
+    //kick the player off
+    public void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.SetParent(null);
+        }
     }
 }
