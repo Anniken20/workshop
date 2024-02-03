e24481d3621f40e1345f9f262dc0f0ac0a04ac1b
CADEN <ca747322@ucf.edu>
2024-02-03 18:18:46 -0500

    Teleport controller locks player animation

--- Assets/Scripts/TeleportController.cs
+++ Assets/Scripts/TeleportController.cs
@@ -67,12 +67,7 @@ public class TeleportController : MonoBehaviour
 
         // Disable character control
         characterController.enabled = false;
-        ThirdPersonController.Main.LockPlayerForDuration(walkDuration + initialDelay);
-        Transform camFollowTransform = CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera.Follow;
-        Vector3 camOffset = CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera.VirtualCameraGameObject.transform.localPosition;
-        Quaternion camRotation = CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera.VirtualCameraGameObject.transform.rotation;
-        CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera.Follow = gameObject.transform;
-        CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera.LookAt = gameObject.transform;
+        ThirdPersonController.Main.LockPlayerForDuration(6*walkDuration + initialDelay);
 
         StartCoroutine(FadeToBlack());
 
@@ -135,10 +130,6 @@ public class TeleportController : MonoBehaviour
 
         // Teleport the player to the final destination
         player.transform.position = teleportDestination;
-        CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera.VirtualCameraGameObject.transform.SetPositionAndRotation(
-            ThirdPersonController.Main.transform.position + camOffset, camRotation);
-        CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera.Follow = camFollowTransform;
-        CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera.LookAt = camFollowTransform;
 
         // Move the player for an additional distance after teleporting
         Vector3 moveAfterTeleportDestination = player.transform.position + player.transform.forward * moveAfterTeleportDistance;
