ed501b812eac9e685e014033e592ac540afa41a5
Amadin <111744298+Anniken20@users.noreply.github.com>
2024-02-03 23:49:44 +0100

    mat switch

--- Assets/Scripts/MaterialSwitch.cs
+++ Assets/Scripts/MaterialSwitch.cs
@@ -3,40 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-//NO MORE BUGS WOOO
-//All numbers can be edited but too different can make it cause issues in ghost controller too
-//BE AWARE
-
 public class MaterialSwitch : MonoBehaviour
-
 {
     public CharacterMovement iaControls;
     private InputAction phase;
-    //public Material material1;
-    //public Material ;
     private Shader shaderOG;
     private Shader shaderGhost;
-    public float switchInterval = 5f; // Time interval for material switching in seconds
-    //public KeyCode switchKey = KeyCode.T; // Key to initiate material switching
-    public float switchDelay = 5f; // Delay in seconds before switching is allowed again
-
+    public float switchInterval = 5f;
     private Renderer rend;
+    private Collider coll;
     private bool canSwitch = true;
-
-    private bool baseShaderActive = true;
-    
-    //please be pushed im begging
 
     private void Start()
     {
         rend = GetComponentInChildren<Renderer>();
-       // rend.material = material1; // Initialize with Material1
+        coll = GetComponent<Collider>(); // Assuming the object has a collider
+
         shaderGhost = Shader.Find("Shader Graphs/Ghost Shader");
         shaderOG = Shader.Find("Shader Graphs/LIT TOON");
-
     }
 
-    
     private void Update()
     {
         if (phase.triggered)
@@ -48,57 +34,59 @@ public class MaterialSwitch : MonoBehaviour
             }
         }
     }
-    
-
-   /* public void ToggleShader()
-    {
-        if (baseShaderActive)
-        {
-           // baseMatActive = false;
-            rend.material = material2;
-        } 
-        else
-        {
-            // baseMatActive = true;
-            rend.material = material1;
-        }
-    }*/
 
     public void SwitchMaterial()
     {
-       Renderer[] allMats = rend.GetComponentsInChildren<Renderer>();
-       
-       foreach (Renderer mat in allMats)
-       {
-        if (mat.material.shader != null)
+        Renderer[] allMats = rend.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer mat in allMats)
         {
-            mat.material.shader = shaderGhost; // change to ghostShader
+            if (mat.material.shader != null)
+            {
+                mat.material.shader = shaderGhost;
+            }
         }
-       }
+
+        // Disable collider's interaction with other objects when switching to ghost shader
+        if (coll != null)
+        {
+            coll.isTrigger = true;
+        }
     }
 
     private IEnumerator SwitchCooldown()
     {
-        Renderer[] allMats = rend.GetComponentsInChildren<Renderer>();
         canSwitch = false;
         yield return new WaitForSeconds(switchInterval);
-        
+
+        // Enable collider's interaction with other objects when switching back to original shader
+        if (coll != null)
+        {
+            coll.isTrigger = false;
+        }
+
+        Renderer[] allMats = rend.GetComponentsInChildren<Renderer>();
         foreach (Renderer mat in allMats)
-       {
-            mat.material.shader = shaderOG; // change back
-       }
+        {
+            mat.material.shader = shaderOG;
+        }
+
         canSwitch = true;
     }
 
-    private void Awake(){
+    private void Awake()
+    {
         iaControls = new CharacterMovement();
     }
-    private void OnEnable(){
-        phase = iaControls.CharacterControls.Phase;
 
+    private void OnEnable()
+    {
+        phase = iaControls.CharacterControls.Phase;
         phase.Enable();
     }
-    private void OnDisable(){
+
+    private void OnDisable()
+    {
         phase.Disable();
     }
 }
