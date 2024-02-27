4c1f19f5cd022f14b6879da1b04cca3a2fb8bb70
Ethan_Branham <122489064+EtanX3@users.noreply.github.com>
2024-02-26 23:39:55 -0500

    bunch of stuff build prep

--- Assets/Scripts/Enemies/Minor Enemies/AggroActivationTrigger.cs
+++ Assets/Scripts/Enemies/Minor Enemies/AggroActivationTrigger.cs
@@ -7,22 +7,36 @@ public class AggroActivationTrigger : MonoBehaviour
 {
     //make sure functions are override instead of void
     public GameObject[] aggroArray;
+    public bool deactivateOnEnter;
 
     private void Start()
     {
-        foreach (GameObject aggro in aggroArray)
-        {
-            aggro.SetActive(false);
-        }
+            foreach (GameObject aggro in aggroArray)
+            {
+                aggro.SetActive(false);
+            }
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-            foreach (GameObject aggro in aggroArray)
+            if(deactivateOnEnter)
             {
-                aggro.SetActive(true);
+                foreach (GameObject aggro in aggroArray)
+                {
+                    aggro.SetActive(false);
+                }
+               
             }
+            else
+            {
+                foreach (GameObject aggro in aggroArray)
+                {
+                    aggro.SetActive(true);
+                }
+            }
+
         }
     }
 }
