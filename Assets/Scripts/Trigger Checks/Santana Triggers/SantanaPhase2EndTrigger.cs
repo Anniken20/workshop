3f18db6fde012635e170b1bbab64409b89563705
CADEN <ca747322@ucf.edu>
2024-03-12 16:09:27 -0400

    porgess sanatan yeay

--- Assets/Scripts/Trigger Checks/Santana Triggers/SantanaPhase2EndTrigger.cs
+++ Assets/Scripts/Trigger Checks/Santana Triggers/SantanaPhase2EndTrigger.cs
@@ -8,12 +8,28 @@ public class SantanaPhase2EndTrigger : MonoBehaviour
     public UnityEvent onSantanaHit;
     private void OnTriggerEnter(Collider other)
     {
+        //Debug.Log("trigger hit " + other.gameObject);
         Salesman s = other.gameObject.GetComponent<Salesman>();
         if (s != null)
         {
-            s.TakeDamage((int)(s.maxHealth / 2));
-            onSantanaHit.Invoke();
             Debug.Log("santana hit cross trigger");
+            s.TakeDamage((int)(s.maxHealth / 2));
+            onSantanaHit?.Invoke();
         }
     }
+
+    /*
+    private void OnCollisionEnter(Collision other)
+    {
+        //Debug.Log("collision hit " + other.gameObject);
+        Salesman s = other.gameObject.GetComponent<Salesman>();
+        if (s != null)
+        {
+
+            Debug.Log("santana hit cross trigger");
+            s.TakeDamage((int)(s.maxHealth / 2));
+            onSantanaHit.Invoke();
+        }
+    }
+    */
 }
