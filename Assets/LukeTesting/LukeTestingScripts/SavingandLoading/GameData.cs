29739a69c62becae30dd355c9323f3eed6b44e52
Luke <eeveetrainer51@gmail.com>
2024-03-14 19:21:49 -0400

    loading checkpoints

--- Assets/LukeTesting/LukeTestingScripts/SavingandLoading/GameData.cs
+++ Assets/LukeTesting/LukeTestingScripts/SavingandLoading/GameData.cs
@@ -7,13 +7,14 @@ public class GameData
    public int coins;
    public int levelComplete;
    public int testingSave;
-   Dictionary<string, int> checkpointSaving = new Dictionary<string, int>();
+   public Vector3 savedCheckpoint;
+   public string checkpointScene;
 //These will be the default values when the game is loaded with no save data to pull from.
    public GameData(){
         this.coins = 0;
         this.levelComplete = 0;
         this.testingSave = 0;
-        this.checkpointSaving.Add("Level", 0);
-        this.checkpointSaving.Add("Checkpoint", 0);
+        this.savedCheckpoint = Vector3.zero;
+        this.checkpointScene = "Tutorial 1";
    } 
 }
