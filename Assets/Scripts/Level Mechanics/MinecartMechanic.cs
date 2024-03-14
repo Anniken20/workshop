e62ecf43b9867fe2d959488945159330f5cd4865
CADEN <ca747322@ucf.edu>
2024-03-14 18:50:08 -0400

    minecart track switching progress

--- Assets/Scripts/Level Mechanics/MinecartMechanic.cs
+++ Assets/Scripts/Level Mechanics/MinecartMechanic.cs
@@ -8,17 +8,45 @@ using StarterAssets;
 public class MinecartMechanic : MonoBehaviour
 {
     private SplineAnimate splineAnimator;
+    public GameObject riderTransform;
+    private CharacterController characterController;
+    private Coroutine rideRoutine;
 
     //start and stop for a frame to fix rotation bug
     private void Start()
     {
         splineAnimator = GetComponent<SplineAnimate>();
-        splineAnimator.Updated += OnUpdateSpline;
+    }
+
+    public void StartRide()
+    {
+        splineAnimator.Play();
+        rideRoutine = StartCoroutine(RideRoutine());
+    }
+
+    private IEnumerator RideRoutine()
+    {
+        characterController = ThirdPersonController.Main.GetComponent<CharacterController>();
+        characterController.enabled = false;
+        while (true)
+        {
+            yield return null;
+            ThirdPersonController.Main.transform.position = riderTransform.transform.position;
+        }
     }
 
     public void OnUpdateSpline(Vector3 pos, Quaternion rotation)
     {
         if(splineAnimator.NormalizedTime > 0.99f)
+        {
+            splineAnimator.NormalizedTime = 1f;
+            OnEnd();
+        }
+    }
+
+    public void CheckSplineTime()
+    {
+        if (splineAnimator.NormalizedTime > 0.99f)
         {
             splineAnimator.NormalizedTime = 1f;
             OnEnd();
@@ -32,7 +60,18 @@ public class MinecartMechanic : MonoBehaviour
 
     public void OnEnd()
     {
+        StopCoroutine(rideRoutine);
+    }
 
+    public void SwitchTracks(GameObject newTrack)
+    {
+        SplineContainer spl = newTrack.gameObject.GetComponent<SplineContainer>();
+        if (spl == null) Debug.LogWarning("No spline component on " + newTrack.name);
+        else
+        {
+            splineAnimator.Container = spl;
+            //splineAnimator.NormalizedTime = spl.
+        }
     }
  
 }
