8271a92df6a3f437a4b23ef6c92e2cbcf33e2de6
bvo29 <bvocubs@gmail.com>
2024-03-16 20:01:39 -0400

    Increased ammo

--- Assets/Scripts/Menus/BulletHUD.cs
+++ Assets/Scripts/Menus/BulletHUD.cs
@@ -11,44 +11,59 @@ using UnityEngine;
 
 public class BulletHUD : MonoBehaviour
 {
-    private GameObject singleBullet;
+   private GameObject singleBullet;
     private float offset = 25f;
     private int totalAmmo;
     private int currentAmmo;
-    private GameObject[] bulletsArray;
-
+    private List<GameObject> bulletsList = new List<GameObject>();
 
     public void DrawBullets()
     {
         currentAmmo = totalAmmo;
         singleBullet = transform.GetChild(0).gameObject;
-        bulletsArray = new GameObject[totalAmmo];
-        bulletsArray[0] = singleBullet;
+        singleBullet.SetActive(true); // Ensure the initial bullet is active.
+
+        bulletsList.Clear(); // Clear the list before redrawing.
+        bulletsList.Add(singleBullet); // Add the first bullet to the list.
 
         for (int i = 1; i < totalAmmo; ++i)
         {
-            GameObject sb = Instantiate(singleBullet, singleBullet.transform);
-            sb.transform.localPosition += new Vector3(offset * i, 0);
-            bulletsArray[i] = sb;
+            GameObject sb = Instantiate(singleBullet, transform, false);
+            sb.transform.localPosition = singleBullet.transform.localPosition + new Vector3(offset * i, 0, 0);
+            bulletsList.Add(sb);
         }
     }
 
-    public void StartBulletHUD(int totalAmmo)
+    public void StartBulletHUD(int ammo)
     {
-        this.totalAmmo = totalAmmo;
+        totalAmmo = ammo;
         DrawBullets();
     }
 
     public void SubtractBulletHUD()
     {
-        currentAmmo--;
-        bulletsArray[currentAmmo].SetActive(false);
+        if (currentAmmo > 0)
+        {
+            currentAmmo--;
+            bulletsList[currentAmmo].SetActive(false);
+        }
     }
 
     public void AddBulletHUD()
     {
-        bulletsArray[currentAmmo].SetActive(true);
-        currentAmmo++;
+        if (currentAmmo < totalAmmo)
+        {
+            bulletsList[currentAmmo].SetActive(true);
+            currentAmmo++;
+        }
+        else if (currentAmmo == totalAmmo)
+        {
+            GameObject newBullet = Instantiate(singleBullet, transform, false);
+            newBullet.transform.localPosition = singleBullet.transform.localPosition + new Vector3(offset * totalAmmo, 0, 0);
+            newBullet.SetActive(true);
+            bulletsList.Add(newBullet);
+            currentAmmo++;
+            totalAmmo++;
+        }
     }
-
 }
