c378305e3043f7c6df41553aa852096df969f05a
LucasP0326 <2002lucassanchez@gmail.com>
2024-03-25 11:51:30 -0400

    Loading Screen Commit
    We got Loading screen now and I'm trying to fix that dumb barn

--- Assets/Scripts/SceneTeleport.cs
+++ Assets/Scripts/SceneTeleport.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -16,8 +18,28 @@ public class SceneTeleport : MonoBehaviour
             {
                 dataManager.SaveGame();
             }
-            SceneManager.LoadScene(scene_name);
+            LoadScene(scene_name);
+        } 
+    }
+
+    public void LoadScene(string scene_name)
+    {
+        StartCoroutine(LoadSceneAsync(scene_name));
+    }
+
+    IEnumerator LoadSceneAsync(string scene_name)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene_name);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progressValue=Mathf.Clamp01(operation.progress/0.9f);
+
+            LoadingBarFill.fillAmount = progressValue;
+
+            yield return null;
         }
-        
     }
 }
