213a67153a3c4d324c88fa841f09b0a1a853052d
LucasP0326 <2002lucassanchez@gmail.com>
2024-03-25 11:20:15 -0400

    Temp Commit
    Adding some last things maybe

--- Assets/Scripts/SceneTeleport.cs
+++ Assets/Scripts/SceneTeleport.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneTeleport : MonoBehaviour
 {
+    public GameObject LoadingScreen;
+    public Image LoadingBarFill;
     public string scene_name;
     public DataManager dataManager;
     public void OnTriggerEnter(Collider other)
