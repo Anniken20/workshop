34069b97d26ef8868c36abf0593ee846704b7192
LucasP0326 <2002lucassanchez@gmail.com>
2024-04-11 17:53:34 -0400

    Level 2 audio changes
    Gonna send a transfer request to level 2

--- Assets/BrianTestScripts/BrianTestScripts/KeyController.cs
+++ Assets/BrianTestScripts/BrianTestScripts/KeyController.cs
@@ -6,13 +6,22 @@ public class KeyController : MonoBehaviour
 {
     [SerializeField] InventoryManager.AllItems itemType;
      [SerializeField] GameObject collectedItemImage;
+     private AudioSource audioSource;
+     public AudioClip pickupSound;
 
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
     //pickup key
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Player"))
         {
             InventoryManager.Instance.AddItem(itemType);
+            if (audioSource != null && pickupSound != null)
+                AudioManager.main.Play(AudioManager.AudioSourceChannel.SFX, pickupSound);
             Destroy(gameObject);
              ShowCollectedItemImage(); 
              Invoke("HideCollectedItemImage", 60f);
