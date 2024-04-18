dee6535e5b37e781771ae0c83551c80e27901c68
welsewul <klolita7878@gmail.com>
2024-04-18 14:52:11 -0400

    dissolver on death

--- Assets/Dissolver.cs
+++ Assets/Dissolver.cs
@@ -1,18 +1,55 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Dissolver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Renderer[] childRenderers;
+    private Material[] childMaterials;
+    public float duration = 3.0f; 
+
+    private void Start()
     {
-        
+        childRenderers = GetComponentsInChildren<Renderer>();
+        childMaterials = new Material[childRenderers.Length];
+
+        for (int i = 0; i < childRenderers.Length; i++)
+        {
+            Material mat = childRenderers[i].material;
+
+            if (mat.shader.name == "ShaderGraph/GhostUnlitURPShader") 
+            {
+                childMaterials[i] = mat;
+            }
+        }
+        StartCoroutine(DissolveMaterials());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator DissolveMaterials()
     {
-        
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            float dissolveValue = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+
+            for (int i = 0; i < childMaterials.Length; i++)
+            {
+                if (childMaterials[i] != null)
+                {
+                    childMaterials[i].SetFloat("Dissolve", dissolveValue); 
+                }
+            }
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        for (int i = 0; i < childMaterials.Length; i++)
+        {
+            if (childMaterials[i] != null)
+            {
+                childMaterials[i].SetFloat("Dissolve", 0f); 
+            }
+        }
     }
 }
