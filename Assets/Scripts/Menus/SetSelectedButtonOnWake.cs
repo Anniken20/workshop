95d8b11918d5f0dae03e2fc9d94174dd9ad39dff
Luke <eeveetrainer51@gmail.com>
2024-04-20 19:33:41 -0400

    main menu navigation

---
+++ Assets/Scripts/Menus/SetSelectedButtonOnWake.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SetSelectedButtonOnWake : MonoBehaviour
+{
+    public GameObject button;
+    private void Start(){
+        
+    }
+    public void OnEnable(){
+        StartCoroutine(Wait());
+    }
+    private IEnumerator Wait(){
+        yield return new WaitForSeconds(0.01f);
+        //Debug.Log("I am actually doing something");
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(button);
+        FindObjectOfType<MenuManager>().currentHovered = button;
+    }
+    private void OnDisable(){
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+}
