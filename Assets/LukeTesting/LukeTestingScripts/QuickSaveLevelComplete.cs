3bd34df0f48d89953839ebbda9683c8034d34e36
frougs <eeveetrainer51@gmail.com>
2024-04-25 11:03:01 -0400

    Update QuickSaveLevelComplete.cs

--- Assets/LukeTesting/LukeTestingScripts/QuickSaveLevelComplete.cs
+++ Assets/LukeTesting/LukeTestingScripts/QuickSaveLevelComplete.cs
@@ -6,14 +6,18 @@ public class QuickSaveLevelComplete : MonoBehaviour, IDataPersistence
 {
     [SerializeField] int thisLevel;
     public DataManager dataManager;
+    private bool isDead = false;
     public void LoadData(GameData data){
     }
     public void SaveData(ref GameData data){
-        data.levelComplete = this.thisLevel;
+        if(isDead){
+            data.levelComplete = this.thisLevel;
+        }
     }
     public void DeathSave(){
         if(dataManager != null)
             {
+                isDead = true;
                 dataManager.SaveGame();
             }
     }
